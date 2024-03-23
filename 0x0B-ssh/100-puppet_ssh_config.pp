133f263a02fb9ef1bc7db7fee668b8995afb1365
Adegboyega Ademola <a.adegboyega11@gmail.com>
2024-03-23 00:42:43 -0700

    0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+}
